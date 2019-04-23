da7a551f65ecd82c6111c8299b9b5b77140a786a
Adrian <adr.pawlak@gmail.com>
2019-04-23 10:35:37 +0200

    DI-52

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,28 @@ $(document).ready(() => {
     const box = new Box(document.getElementsByClassName('js-box')[0]);
     box.init();
 });
+
+const users = './json/users.json';
+const emoji = './json/emojis.json';
+const cards = './json/cards.json';
+const boxes = './json/boxes.json';
+let cardsObj;
+
+// fetch //
+fetch(cards)
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+            console.log('response');
+        }
+        throw new Error();
+        console.log('error z else');
+    })
+    .then((data) => {
+        cardsObj = data;
+        console.log(obj);
+        console.log(data[0].id);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
