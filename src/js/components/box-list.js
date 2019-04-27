c46cb0dab111ffba5257b9f7550cf22d97efbae6
bebeto9009 <adr.pawlak@gmail.com>
2019-04-28 00:09:09 +0200

    generate boxes

---
+++ src/js/components/box-list.js
@@ -0,0 +1,64 @@
+class BoxList {
+    constructor(boxList) {
+        this.boxList = boxList;
+        this.boxes = './json/boxes.json';
+        this.cardsObj; // empty array for json
+    }
+
+    createBox(boxTitle) {
+        let item = document.createElement('a');
+        item.classList.add('grid__item');
+        item.href = '/single-box.html';
+
+        let domoBox = document.createElement('div');
+        domoBox.classList.add('domo-box');
+        item.appendChild(domoBox);
+
+        let domoName = document.createElement('h3');
+        domoName.classList.add('domo-box__name');
+        domoBox.appendChild(domoName);
+        domoName.innerText += boxTitle;
+
+        this.boxList.appendChild(item);
+    }
+
+    generateListBox(cards) {
+        for (let i = 0; i < cards.length; i++) {
+            this.createBox(cards[i].name);
+        }
+    }
+
+    fetch() {
+        fetch(this.boxes)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error();
+                }
+            })
+            .then(response => {
+                this.cardsObj = response;
+                this.generateListBox(this.cardsObj);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    events() {
+        this.fetch();
+    }
+
+    init() {
+        if (this.boxList) {
+            try {
+                this.events();
+            } catch (e) {
+                console.warn(e);
+            }
+        }
+    }
+}
+
+export default BoxList;
--- src/js/index.js
+++ src/js/index.js
@@ -2,36 +2,22 @@ import $ from 'jquery';
 
 import Modal from './components/modal';
 import Box from './components/box';
+import BoxList from './components/box-list';
 
 $(document).ready(() => {
+    /* jsons for other boxes
+    const users = './json/users.json';
+    const emoji = './json/emojis.json';
+    const cards = './json/cards.json';
+    */
+
     const modal = new Modal(document.getElementById('btn__new-domo'));
     modal.init();
 
     const box = new Box(document.getElementsByClassName('js-box')[0]);
     box.init();
+
+    const boxList = new BoxList(document.querySelector('.js-box-list'));
+    boxList.init();
 });
 
-const users = './json/users.json';
-const emoji = './json/emojis.json';
-const cards = './json/cards.json';
-const boxes = './json/boxes.json';
-let cardsObj;
-
-// fetch //
-fetch(cards)
-    .then((response) => {
-        if (response.ok) {
-            return response.json();
-            console.log('response');
-        }
-        throw new Error();
-        console.log('error z else');
-    })
-    .then((data) => {
-        cardsObj = data;
-        console.log(obj);
-        console.log(data[0].id);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
